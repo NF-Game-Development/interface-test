5bb2732292b6860f73c98069fc2e86aa60b68ead
Christian Lenar Santiago <ethan_santiago@neun.ph>
2025-04-15 11:56:54 +0800

    Interface Activity 2: Changed Interact Range and Pickup Range to Stat enum

--- Assets/Scripts/Gameplay/Player/PlayerInteraction.cs
+++ Assets/Scripts/Gameplay/Player/PlayerInteraction.cs
@@ -5,23 +5,23 @@ using UnityEngine.Serialization;
 
 public class PlayerInteraction : MonoExt
 {
-    [SerializeField] private float _interactRange;
+    [SerializeField] private Stat _interactRange;
     [SerializeField] private LayerMask _interactableLayer;
     [SerializeField] private Camera _topDownCamera;
-    [SerializeField] private float _collectRadius;
+    [SerializeField] private Stat _collectRadius;
     [SerializeField] private SphereCollider _collider;
     
     [SerializeField] private List<ItemDropExtendableEnum> _collectedObjects = new List<ItemDropExtendableEnum>();
 
     private void Start()
     {
-        _collider.radius = _collectRadius;
+        _collider.radius = _collectRadius.Value;
     }
 
     private void Update()
     {
         //for testing only
-        _collider.radius = _collectRadius;
+        _collider.radius = _collectRadius.Value;
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
@@ -33,9 +33,9 @@ public class PlayerInteraction : MonoExt
     {
         Ray ray = _topDownCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        Debug.DrawRay(ray.origin, ray.direction * _interactRange, Color.green, 1f);
+        Debug.DrawRay(ray.origin, ray.direction * _interactRange.Value, Color.green, 1f);
 
-        if (Physics.Raycast(ray, out hit, _interactRange, _interactableLayer))
+        if (Physics.Raycast(ray, out hit, _interactRange.Value, _interactableLayer))
         {
             Debug.Log("Hit: " + hit.collider.name);
             
