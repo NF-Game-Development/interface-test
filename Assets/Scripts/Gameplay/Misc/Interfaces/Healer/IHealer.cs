a3565a6b02224382bb81736b9019db46f4f04337
NF-Jhan-Perez <jhan_perez@neun.ph>
2025-07-30 14:32:40 +0800

    Interface Activity 1: Added additional Universal Interface

---
+++ Assets/Scripts/Gameplay/Misc/Interfaces/Healer/IHealer.cs
@@ -0,0 +1,4 @@
+public interface IHealer
+{
+    //TODO: Add Function here to be used by Healer for the BaseHero
+}
---
+++ Assets/Scripts/Gameplay/Misc/Interfaces/Universal Interface/IHealable.cs
@@ -0,0 +1,4 @@
+public interface IHealable
+{
+    void ApplyHeal(int addHealth);
+}
