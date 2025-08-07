7d33355c2463b6039912133c3435b0e22755744f
NF-Jhan-Perez <jhan_perez@neun.ph>
2025-08-07 10:52:09 +0800

    Interface Activity 3: Added Tank Moving Animation

--- Assets/_Project/Scripts/Hero/TankHero.cs
+++ Assets/_Project/Scripts/Hero/TankHero.cs
@@ -1,6 +1,48 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class TankHero : BaseHero
 {
+    [TabGroup("Melee Hit Location")] [SerializeField] private Transform _hitLocation;
+    
+    private float _attackRadius;
+    private float _damage;
+    
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        InitializeDamageValues();
+    }
+
+    private void InitializeDamageValues()
+    {
+        _damage = HeroScriptableObject.BaseAttack;
+        _attackRadius = HeroScriptableObject.MeleeRadius;
+    }
+    
+    public override void PerformAttack()
+    {
+        Collider[] hits = Physics.OverlapSphere(_hitLocation.transform.position, _attackRadius);
+
+        foreach (var hit in hits)
+        {
+            if (hit.transform.root.TryGetComponent<IDamageable>(out var damageable))
+            {
+                Attack(damageable);
+            }
+        }
+    }
+    
+    public override void Attack(IDamageable damageable)
+    {
+        damageable.ApplyDamage(_damage);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_hitLocation.transform.position, _attackRadius);
+    }
     
 }
